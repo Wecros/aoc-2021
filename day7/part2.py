921cab3ab6aa0f6b20ff78a7f55eb2446804d4d2
mafilip <mafilip@redhat.com>
2021-12-07 14:59:15 +0100

    Add day7 solution

---
+++ day7/part2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import fileinput
+import sys
+
+
+def main():
+    positions = list(get_input())
+    min_pos = min(positions)
+    max_pos = max(positions)
+
+    fuel_positions = {}
+    for pos in range(min_pos, max_pos + 1):
+        fuel_positions[pos] = calculate_position(positions, pos)
+    print(min(fuel_positions.values()))
+
+
+def calculate_position(positions, target_pos):
+    fuel_positions = [sum(range(abs(pos - target_pos) + 1)) for pos in positions]
+    return sum(fuel_positions)
+
+
+def get_input():
+    return map(int, next(fileinput.input()).strip().split(","))
+
+
+if __name__ == "__main__":
+    main()
